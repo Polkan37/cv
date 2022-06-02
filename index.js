29ebd5704b028328dd0f407252a5f9c322213597
Borys Lymar <borys.lymar@gopuff.com>
2022-06-02 18:55:52 +0300

    refactor structure

---
+++ index.js
@@ -0,0 +1,8 @@
+import('./components/accordion')
+import('./components/contact-form')
+import('./components/header')
+import('./components/menu')
+import('./components/modal')
+import('./components/swiper')
+import('./components/tabs')
+import('./components/theme')
